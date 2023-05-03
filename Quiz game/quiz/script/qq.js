9d570216457ba288c303b99475955909e5cff475
Lucas Ribeiro <82050425+048lucas@users.noreply.github.com>
2023-05-03 18:02:40 -0300

    Add files via upload
    Atualização dia 03/05/2023
    
    
    Script rodando corretamente

---
+++ Quiz game/quiz/script/qq.js
@@ -0,0 +1,37 @@
+export default [
+    {
+      question: "Quem chegou ao brasil em 1500?",
+      answers: [
+        { option: "Pedro Alvares", correct: true },
+        { option: "Pedro Colombo", correct: false },
+        { option: "Povos indigenas", correct: false },
+        
+        
+        
+      ],
+    },
+    {
+      question: "Qual nome do primeiro chefe de estado do prasil(primeiro presidente)?",
+      answers: [
+        { option: "Marechal teodoro martins", correct: false },
+        { option: "Major Fonseca", correct: false },
+        { option: "Teodoro da fonseca", correct: true },
+      ],
+    },
+    {
+      question: "Em que ano foi criado o hino da independecia?",
+      answers: [
+        { option: "7 de setembro de 1822", correct: true },
+        { option: "7 de setembro de 1821", correct: false },
+        { option: "30 de fevereiro de 1822", correct: false },
+      ],
+    },
+    {
+      question: "Quem foi oswaldo Cruz?",
+      answers: [
+        { option: "Oswaldo Cruz foi um sanitarista", correct: false },
+        { option: "Foi um presidente da republica", correct: false },
+        { option: "Um ativista politico no regime militar", correct: true },
+      ],
+    },
+  ];
